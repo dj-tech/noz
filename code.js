e5797c637b0c274fe0eb39c625e9a3d6a3e3455b
dj-tech <www.giuseppe@dellavedova.org>
2017-12-08 13:43:18 +0100

    Ajax and Jquery

--- code.js
+++ code.js
@@ -134,11 +134,13 @@ alert(prodotti);
 alert("soldi");
 alert(soldi);
 
+if (a == true) {
 $.ajax({
-  method: "POST",
-  url: "some.php",
-  data: { name: prompt("username"), private_point: [terreno,prodotti,soldi] }
-})
-  .done(function( msg ) {
-    alert( "Data Saved: " + msg );
-});
+  	method: "POST",
+  	url: "some.php",
+  	data: { name: prompt("username"), private_point: [terreno,prodotti,soldi] }
+	})
+  	.done(function( msg ) {
+    		alert( "Data Saved: " + msg );
+	});
+}
